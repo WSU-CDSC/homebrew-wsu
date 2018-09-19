1220be532cafaca42f565ebd1183cff0c6da7548
Andrew Weaver <theandrewjw@gmail.com>
2018-09-19 14:11:49 -0700

    start empty homebrew formula

---
+++ wsumicroservices.rb
@@ -0,0 +1,14 @@
+require "formula"
+class Wsumicroservices < Formula
+  desc "Microservices used in digital preservation at Washington State University"
+  homepage ""
+  url ""
+  version ""
+  sha256 ""
+  head ""
+  
+  
+  def install
+    bin.install ""
+  end
+end
