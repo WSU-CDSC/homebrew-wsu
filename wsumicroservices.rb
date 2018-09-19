f668d48269b28c11e6d312ccd26ae10c0e3ad865
Andrew Weaver <theandrewjw@gmail.com>
2018-09-19 15:46:52 -0700

    dependencies

--- wsumicroservices.rb
+++ wsumicroservices.rb
@@ -6,7 +6,10 @@ class Wsumicroservices < Formula
   version "2018-09-19"
   sha256 "c9ca76e4a389d6f4c564b00c5005a8e77e9a5cb005a3ef446c88a307c2abe3fd"
   head "https://github.com/WSU-CDSC/microservices.git"
+  revision 1
   
+  depends_on "hashdeep"
+  depends_on "bagit"
   
   def install
     bin.install "aip2b2.rb"
