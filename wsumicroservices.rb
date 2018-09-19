954eca32137ade09d3b2a6feacc06c35aae56e8e
Andrew Weaver <theandrewjw@gmail.com>
2018-09-19 15:31:37 -0700

    update with release

--- wsumicroservices.rb
+++ wsumicroservices.rb
@@ -2,9 +2,9 @@ require "formula"
 class Wsumicroservices < Formula
   desc "Microservices used in digital preservation at Washington State University"
   homepage ""
-  url ""
-  version ""
-  sha256 ""
+  url "https://github.com/WSU-CDSC/microservices/archive/2018-09-19.tar.gz"
+  version "2018-09-19"
+  sha256 "c9ca76e4a389d6f4c564b00c5005a8e77e9a5cb005a3ef446c88a307c2abe3fd"
   head "https://github.com/WSU-CDSC/microservices.git"
   
   
